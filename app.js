3debfde0504d1495e40f6e7471e4d518cff3d2b8
abhinavgadekar <abhinavgadekar13@gmail.com>
2025-03-25 22:57:38 +0530

    update

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = 3000
+app.use(express.json());
+const Groq = require('groq-sdk');
 
-app.get('/student', (req, res) => {
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+app.post('/prompt', async (req, res) => {
 
-    res.status(200).json({
-        msg: 'Hello i am get student api',
-        studname:"Ram"
+    const chatCompletion = await groq.chat.completions.create({
+        messages: [{
+            role: "user", content: req.body.prompt,
+        },],
+        model: "llama3-8b-8192",
     })
-})
-app.get('/employee', (req, res) => {
+        .then((chatCompletion) => {
+            // console.log(chatCompletion.choices[0]?.message?.content || "");
+            res.status(200).json({
+                msg: chatCompletion.choices[0].message.content
+            })
+        });
 
-    res.status(200).json({
-        msg: 'Hello i am get employee api',
-        studname:"Amar"
-    })
-})
-app.post('/', (req, res) => {
 
-    res.status(201).json({
-        msg: 'This is post api'
-    })
 })
 
-app.put('/', (req, res) => {
-
-    res.status(201).json({
-        msg: 'This is put api'
-    })
-})
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
 })
